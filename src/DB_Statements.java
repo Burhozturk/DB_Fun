68c99bc96bc670a6cb6f08837ecbb83a001bc145
Mikkel Sørensen <mik4211@edu.easj.dk>
2017-11-08 09:51:26 +0100

    Version 2

--- src/DB_Statements.java
+++ src/DB_Statements.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DB_Statements {
 
@@ -11,6 +8,9 @@ public class DB_Statements {
     private static Connection con = Dbconnector.connect();
     // Declare a result set
     private static ResultSet rs = null;
+    // Declare a prepared statement
+    private static PreparedStatement pst = null;
+
 
     // Method to create a new database
     public void createNewDB() {
@@ -28,7 +28,8 @@ public class DB_Statements {
             ex.printStackTrace();
         }
     }
-        // method to use a datbase
+
+    // method to use a datbase
     public void useDB(String dbName) {
         //statement
         String query = "use thisdatabase";
@@ -44,71 +45,95 @@ public class DB_Statements {
             ex.printStackTrace();
         }
     }
-        // Creates a table
-        public void createTable(String tableName) {
-            //SQL Statement
-            String query = "create table if NOT EXISTS " + tableName +
-                    "(" +
-                    "id int not null auto_increment," +
-                    "myName varchar(28)," +
-                    "address varchar(28)," +
-                    "primary key (id)" +
-                    ")";
-            try {
-                //Connection
-                stat = con.createStatement();
-                //Execute the query
-                stat.executeUpdate(query);
-                System.out.println("\n--Table" + tableName + " created--");
-            } catch (SQLException ex) {
-                System.out.println("\n ---Query didn't execute---");
-                ex.printStackTrace();
-            }
+
+    // Creates a table
+    public void createTable(String tableName) {
+        //SQL Statement
+        String query = "create table if NOT EXISTS " + tableName +
+                "(" +
+                "id int not null auto_increment," +
+                "myName varchar(28)," +
+                "address varchar(28)," +
+                "primary key (id)" +
+                ")";
+        try {
+            //Connection
+            stat = con.createStatement();
+            //Execute the query
+            stat.executeUpdate(query);
+            System.out.println("\n--Table" + tableName + " created--");
+        } catch (SQLException ex) {
+            System.out.println("\n ---Query didn't execute---");
+            ex.printStackTrace();
         }
-        //Method to insert data
-            public void insertData(String tableName) {
-                //SQL query
-                String query = "insert into " + tableName + "(" +
-                        "MyName, address) " +
-                        "values ('Mikkel','My Address'), " +
-                        "('Bob ', 'His address'), " +
-                        "('John', 'Their address')";
-                try {
-                    //Connect
-                    stat = con.createStatement();
-                    //Execute query
-                    stat.executeUpdate(query);
-                    System.out.println("\n --Data inserted into table " + tableName);
-                } catch (SQLException ex) {
-                    //handle exceptions
-                    ex.printStackTrace();
-                }
-            }
-                    // Method to read data from table
-               public void selectFromTable(String tableName){
-             // SQL Statement
-             String query = "Select * from "+ tableName;
-            try{
-                //Connection
-                stat = con.createStatement();
-                //Execute statement
-                rs = stat.executeQuery(query);
-                System.out.println("\nid\t\tMyName\t\taddress\n----------------------------------");
-
-                //get data
-                while(rs.next()){
-                    int id = rs.getInt(1);  // Returns ID(column 1)
-                    String MyName = rs.getString("MyName"); // Returns MyName (column 2)
-                    String address = rs.getString("address"); // Returns address (column 3)
-                    System.out.println(id + "\t\t" + MyName + "\t\t" + address);
-                }
-
-            }
-            catch (SQLException ex){
-                System.out.println("\n--Query didn't execute--");
-                ex.printStackTrace();
-            }
-
-    }
     }
 
+    //Method to insert data
+    public void insertData(String tableName) {
+        //SQL query
+        String query = "insert into " + tableName + "(" +
+                "MyName, address) " +
+                "values ('Mikkel','My Address'), " +
+                "('Bob ', 'His address'), " +
+                "('John', 'Their address')";
+        try {
+            //Connect
+            stat = con.createStatement();
+            //Execute query
+            stat.executeUpdate(query);
+            System.out.println("\n --Data inserted into table " + tableName);
+        } catch (SQLException ex) {
+            //handle exceptions
+            ex.printStackTrace();
+        }
+    }
+
+    // Method to read data from table
+    public void selectFromTable(String tableName) {
+        // SQL Statement
+        String query = "Select * from " + tableName;
+        try {
+            //Connection
+            stat = con.createStatement();
+            //Execute statement
+            rs = stat.executeQuery(query);
+            System.out.println("\nid\t\tMyName\t\taddress\n----------------------------------");
+
+            //get data
+            while (rs.next()) {
+                int id = rs.getInt(1);  // Returns ID(column 1)
+                String MyName = rs.getString("MyName"); // Returns MyName (column 2)
+                String address = rs.getString("address"); // Returns address (column 3)
+                System.out.println(id + "\t\t" + MyName + "\t\t" + address);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("\n--Query didn't execute--");
+            ex.printStackTrace();
+        }
+
+    }
+
+    //Checking user credentials
+    public Boolean checkLogin(String username, String password) {
+        boolean check = false;      // Boolean til at checke om username/password er rigtige. False som default.
+
+        String query = "select * from thisdatabase.user where username = (?) and password = (?)";
+
+        try {
+           // stat = con.createStatement();
+            pst = con.prepareStatement(query);
+           // rs = stat.executeQuery(query);
+            while (rs.next()) {
+                check = true;
+                System.out.println("\n --wew lad u cunt! it works!--");
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("\n --Fukin hell boy you suck");
+            ex.printStackTrace();
+        }
+        return check;
+    }
+
+}
--- src/RunMyShit.java
+++ src/RunMyShit.java
@@ -2,14 +2,14 @@ import java.sql.Connection;
 
 public class RunMyShit {
     public static void main(String[] args) {
-        Connection con = Dbconnector.connect();
-
-        DB_Statements stat = new DB_Statements();
+        //Connection con = Dbconnector.connect();
+        //DB_Statements stat = new DB_Statements();
         //Use DB
-        stat.useDB("thisdatabase");
+        //stat.useDB("thisdatabase");
         //Creates new table
 //        stat.createTable("MyTable");
         //stat.insertData("MyTable");
-        stat.selectFromTable("MyTable");
+        //stat.selectFromTable("MyTable");
+        Login_GUI run = new Login_GUI();
     }
 }
